e8af8f64c93b40638395e7d21f901deda64cdddf
CullingBlade <nvluan.92@gmail.com>
2015-06-10 13:54:58 +0700

    signin-signup-signout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   root  "static_pages#home"
   
   get "home" => "static_pages#home"
   get "help" => "static_pages#help"
   get "about" => "static_pages#about"
+
+  resources :users
 end
