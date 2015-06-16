f5f3276c8c4805f5f4152eb5e36f827d7d89ea33
PhuongTrieu <phuongtrieuduck55@gmail.com>
2015-06-16 10:32:47 +0700

    edit and delete team

--- app/controllers/admin/teams_controller.rb
+++ app/controllers/admin/teams_controller.rb
@@ -20,6 +20,26 @@ class Admin::TeamsController < ApplicationController
     end
   end
 
+  def edit
+    @team = Team.find params[:id]
+  end
+
+  def update
+    @team = Team.find params[:id]
+    if @team.update_attributes team_params
+      flash[:succes] = t "team.updated"
+      redirect_to admin_teams_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Team.find(params[:id]).destroy
+    flash[:succes] = t "team.deleted"
+    redirect_to admin_teams_path
+  end
+
   private
   def team_params
     params.require(:team).permit :name, :description
