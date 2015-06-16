9b3bc47bafd956ed06210e2d439baf368be1f605
biznezo <viettrantrong@gmail.com>
2015-06-16 10:35:55 +0700

    admin add user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,4 +3,24 @@ class Admin::UsersController < ApplicationController
     @users = User.paginate page: params[:page], 
                            per_page: Settings.number_per_page
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t "user.created"
+      redirect_to admin_root_path
+    else
+      flash[:danger] = t "user.create_fail"
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :name, :birthday, :email, :password, :is_admin
+  end
 end
