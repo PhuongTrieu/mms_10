d2e0ea620930187ebf1c272ea9fc4570ce8e563f
PhuongTrieu <phuongtrieuduck55@gmail.com>
2015-06-24 09:34:16 +0700

    choose leader for a team

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,8 @@ class Team < ActiveRecord::Base
   has_many :users, through: :team_users, dependent: :destroy
   has_many :team_users
 
+  belongs_to :leader, class_name: "User", foreign_key: :leader_id
+
   accepts_nested_attributes_for :users, allow_destroy: true
 
   validates :name, presence: true
